2af4e39a56a218adc2e1f70b57c4b9816db5cc26
NareshArjun <88555531+NareshArjun@users.noreply.github.com>
2021-10-01 19:52:42 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,166 +1,77 @@
 "use strict";
 
-(function () {
-  var sliders = document.querySelectorAll(".sliders");
-  var currentSlide = 0;
-  var slideLength = sliders.length;
+let sliderContainers = document.querySelectorAll(".sliders_wrap");
+let containers = [];
+let sliderUpButton, sliderDownButton;
 
-  var dotsWrap = document.querySelector(".dots_wrapper");
-  var sliderUpButton = document.querySelector(".slider_up_button");
-  var sliderDownButton = document.querySelector(".slider_down_button");
+const SliderContainer = function (sliderContainer, currentSlide) {
+  this.sliderContainer = sliderContainer;
+  this.currentSlide = currentSlide;
+  //this.sliders = this.sliderContainer.querySelectorAll(".sliders");
+};
 
-  function slideMovement(slidePosition) {
-    sliders.forEach((slider, index) => {
-      slider.style.transform = `translateY(${100 * (index - slidePosition)}%)`;
-    });
-  }
-
-  function slideForward() {
-    if (currentSlide === slideLength - 1) {
-      currentSlide = 0;
-    } else {
-      currentSlide++;
-    }
-    slideMovement(currentSlide);
-    activeDot(currentSlide);
-  }
-
-  function slideBackward() {
-    if (currentSlide === 0) {
-      currentSlide = slideLength - 1;
-    } else {
-      currentSlide--;
-    }
-    slideMovement(currentSlide);
-    activeDot(currentSlide);
-  }
-
-  function createDots() {
-    sliders.forEach((slider, index) => {
-      dotsWrap.insertAdjacentHTML(
-        "beforeend",
-        `<button class="dots" data-slide="${index}"></button>`
-      );
-    });
-  }
-
-  function activeDot(slidePosition) {
-    document
-      .querySelectorAll(".dots")
-      .forEach((dot) => dot.classList.remove("dots_active"));
-    document
-      .querySelector(`.dots[data-slide="${slidePosition}"]`)
-      .classList.add("dots_active");
-  }
-
-  //Assigning initial positions
-  slideMovement(currentSlide);
-  createDots();
-  activeDot(currentSlide);
-
-  //Movement using Buttons
-  sliderDownButton.addEventListener("click", slideForward);
-  sliderUpButton.addEventListener("click", slideBackward);
-
-  //Movement using Scrolling
-  for (var i = 0; i < slideLength; i++) {
-    (function (i) {
-      sliders[i].addEventListener("wheel", function (e) {
-        if (e.deltaY > 0) {
-          slideForward();
-        } else if (e.deltaY < 0) {
-          slideBackward();
-        }
-      });
-    })(i);
-  }
-
-  //Movement using Dots
-  dotsWrap.addEventListener("click", function (e) {
-    if (e.target.classList.contains("dots")) {
-      var slideIndex = e.target.dataset.slide;
-      slideMovement(slideIndex);
-      activeDot(slideIndex);
-    }
+SliderContainer.prototype.slideMovement = function (slidePosition) {
+  this.sliders = this.sliderContainer.querySelectorAll(".sliders");
+  this.sliders.forEach((slider, index) => {
+    slider.style.transform = `translateY(${100 * (index - slidePosition)}%)`;
+    slider.style.transition = "transform .5s";
+    //slider.classList.remove("active");
+    //if (index === slidePosition) slider.classList.add("active");
   });
+  /*console.log(slidePosition);
+  console.log(sliders[slidePosition]);
+  sliders[slidePosition].classList.add("active");*/
+};
 
-  /*for (var i = 0; i < slideLength; i++) {
-    window.addEventListener("scroll", function (e) {
-      console.log(window.scrollX, window.scrollY, e);
-    });
-  }*/
+SliderContainer.prototype.slideForward = function () {
+  console.log("clicked button");
+  //console.log("SF", currentSlide, "slideLength-1", slideLength - 1);
+  if (this.currentSlide === this.sliders.length - 1) {
+    this.currentSlide = 0;
+  } else {
+    this.currentSlide++;
+  }
+  slideMovement(this.currentSlide);
+  //activeDot(currentSlide);
+  //console.log("SF update", currentSlide);
+};
 
-  /*for (var i = 0; i < sliders.length; i++) {
-    (function (i) {
-      if (i === 0) {
-        var nextSlideCoord = sliders[i + 1].getBoundingClientRect();
-        var PreviousSlideCoord =
-          sliders[sliders.length - 1].getBoundingClientRect();
-      }
-      window.addEventListener("scroll", function () {
-        console.log(window.scrollY, nextSlideCoord, PreviousSlideCoord);
-        window.scrollTo({
-          left: nextSlideCoord.left,
-          top: nextSlideCoord.top,
-          behavior: "smooth",
-        });
-      });
-    })(i);
-  }*/
+SliderContainer.prototype.slideBackward = function () {
+  console.log("clicked button");
+  //console.log("SU", currentSlide);
+  if (this.currentSlide === 0) {
+    this.currentSlide = this.sliders.length - 1;
+  } else {
+    this.currentSlide--;
+  }
+  slideMovement(this.currentSlide);
+  //activeDot(currentSlide);
+  //console.log("SU update", currentSlide);
+};
 
-  /*for (var i = 0; i < slideLength; i++) {
-    (function (i) {
-      sliders[i].addEventListener("wheel", function (e) {
-        console.log(i, sliders[i]);
-        if (i === 0) {
-          if (e.deltaY > 0) {
-            var slideCoordinates = sliders[i + 1].getBoundingClientRect();
-          } else if (e.deltaY < 0) {
-            var slideCoordinates =
-              sliders[sliders.length - 1].getBoundingClientRect();
-          }
-          console.log(
-            i,
-            slideCoordinates,
-            sliders[i],
-            window.scrollX,
-            window.scrollY,
-            slideCoordinates.left,
-            slideCoordinates.top
-          );
-        }
-        window.scrollTo({
-          left: slideCoordinates.left,
-          top: slideCoordinates.top,
-        });
-      });
-    })(i);
-  }*/
+sliderContainers.forEach((sliderContainer, index) => {
+  containers[index] = new SliderContainer(sliderContainer, 0);
+  //containers[index] = container;
+  console.log(containers);
+});
+/*sliderContainers.forEach((container) => {
+  sliderUpButton = container.querySelector(".slider_up_button");
+  sliderDownButton = container.querySelector(".slider_down_button");
 
-  /*window.addEventListener("scroll", function () {
-    console.log(window.scrollY);
+  sliderDownButton.addEventListener("click", container.slideForward);
+  sliderUpButton.addEventListener("click", container.slideBackward);
+});*/
+
+sliderUpButton = document.querySelectorAll(".slider_up_button");
+sliderDownButton = document.querySelectorAll(".slider_down_button");
+
+sliderUpButton.forEach((upButton) => {
+  upButton.addEventListener("click", function (e) {
+    console.log(e);
+    //let container = e.target.offsetParent.offsetParent;
+    this.sliderContainer = e.target.offsetParent.offsetParent;
+    //slideBackward(container);
+    console.log(this.sliderContainer);
+    slideBackward;
   });
-  console.log(sliders);*/
-
-  /*sliders.forEach((slider, index, sliders) => {
-    var nextCoord = sliders[index].getBoundingClientRect();
-    window.addEventListener("scroll", function () {
-      console.log(window.scrollY, nextCoord);
-      window.scrollTo({
-        left: nextCoord.left,
-        top: nextCoord.top,
-        behavior: "smooth",
-      });
-    });
-  });*/
-
-  /*for (var i = 0; i < sliders.length; i++) {
-    (function () {
-      var nextSlideCoord = sliders[i].getBoundingClientRect();
-      window.addEventListener("scroll", function () {
-        console.log(window.scrollY, nextSlideCoord);
-        window.scrollTo(nextSlideCoord.left, nextSlideCoord.top);
-      });
-    })(i);
-  }*/
-})();
+});
